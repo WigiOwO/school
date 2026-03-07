76fdfd2817531e682ca108626c5d2ab86ff2d8b6
WigiOwO <luigiryan008@gmail.com>
2026-03-07 08:03:51 +0800

    Prime Checker

---
+++ Lab_Exercise_1.py
@@ -0,0 +1,29 @@
+from math import sqrt
+num = float(input("Enter a number: "))
+
+verdict = ""
+
+num_sqrt = sqrt(num)
+
+primes = [2, 3, 5]
+
+for i in range(2, int(num_sqrt)):
+    if i % 2 == 0:
+        pass
+    elif i % 3 == 0:
+        pass
+    elif i % 5 == 0:
+        pass
+    else:
+        primes.append(i)
+
+long = range(len(primes))
+
+for prime in primes:
+    if num % prime == 0:
+        verdict = "Not Prime"
+        break
+    else:
+        verdict = "Prime"
+
+print(verdict)
