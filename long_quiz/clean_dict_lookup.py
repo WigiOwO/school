61e1c7ebac778dccd6c42d9484c15ef2369ef01c
WigiOwO <luigiryan008@gmail.com>
2026-03-21 08:39:38 +0800

    insanely efficient

--- long_quiz/clean_dict_lookup.py
+++ long_quiz/clean_dict_lookup.py
@@ -1,2 +1,7 @@
-def get_user_info(data:list, user_id:int):
-    return [i if user_id == i['id'] else {"error":"User not found"} for i in data][0]
+def get_user_info(data:list, user_id:int):return [i if user_id == i['id'] else {"error":"User not found"} for i in data][0]
+
+users = [
+    {"id": 1, "name": "Alice", "age": 25},
+    {"id": 2, "name": "Bob", "age": 30}
+]
+print(get_user_info(users, 1) )
