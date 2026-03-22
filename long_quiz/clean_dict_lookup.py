0efc44ea2a3785b4bc778b17581710bc5bdf713f
WigiOwO <luigiryan008@gmail.com>
2026-03-22 22:36:39 +0800

    one liner

--- long_quiz/clean_dict_lookup.py
+++ long_quiz/clean_dict_lookup.py
@@ -1,7 +1,1 @@
 def get_user_info(data:list, user_id:int):return [i if user_id == i['id'] else {"error":"User not found"} for i in data][0]
-
-users = [
-    {"id": 1, "name": "Alice", "age": 25},
-    {"id": 2, "name": "Bob", "age": 30}
-]
-print(get_user_info(users, 1) )
